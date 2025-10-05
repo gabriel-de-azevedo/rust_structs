74bb1a0d541aa31c59c2febb831f510c8b57da26
Gabriel de Azevedo <184437223+gabriel-de-azevedo@users.noreply.github.com>
2025-10-05 08:20:35 -0300

    feat: build user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123"),
+    );
+
+    println!("{}", user1.active);
+    println!("{}", user1.username);
+    println!("{}", user1.email);
+    println!("{}", user1.sign_in_count);
 }
