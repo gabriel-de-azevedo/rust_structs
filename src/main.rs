6ec020b471161885dddd0c0a2f9f77d48d7897fc
Gabriel de Azevedo <184437223+gabriel-de-azevedo@users.noreply.github.com>
2025-10-05 08:54:54 -0300

    feat: struct update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 struct User {
     active: bool,
-    username: String,
     email: String,
+    username: String,
     sign_in_count: u64,
 }
 
 fn build_user(email: String, username: String) -> User {
     User {
         active: true,
-        username,
         email,
+        username,
         sign_in_count: 1,
     }
 }
@@ -20,8 +20,13 @@ fn main() {
         String::from("someusername123"),
     );
 
-    println!("{}", user1.active);
-    println!("{}", user1.username);
-    println!("{}", user1.email);
-    println!("{}", user1.sign_in_count);
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+
+    println!("{}", user2.active);
+    println!("{}", user2.email);
+    println!("{}", user2.username);
+    println!("{}", user2.sign_in_count);
 }
