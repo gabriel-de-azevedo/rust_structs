2f6cd916a675a0f946b8415659696db07e449285
Gabriel de Azevedo <184437223+gabriel-de-azevedo@users.noreply.github.com>
2025-10-05 09:43:17 -0300

    feat: tuple structs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,9 @@
-struct User {
-    active: bool,
-    email: String,
-    username: String,
-    sign_in_count: u64,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        email,
-        username,
-        sign_in_count: 1,
-    }
-}
+struct Color(i32, i32, i32);
 
 fn main() {
-    let user1 = build_user(
-        String::from("someone@example.com"),
-        String::from("someusername123"),
-    );
+    let black = Color(0, 0, 0);
 
-    let user2 = User {
-        email: String::from("another@example.com"),
-        ..user1
-    };
-
-    println!("{}", user2.active);
-    println!("{}", user2.email);
-    println!("{}", user2.username);
-    println!("{}", user2.sign_in_count);
+    println!("Hue {}", black.0);
+    println!("Value {}", black.1);
+    println!("Chroma {}", black.2);
 }
